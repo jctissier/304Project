4986264d68e3bb9924a9e84b7b60188d6d67ddee
Gary Gao <ubcgary@gmail.com>
2017-11-10 16:33:25 -0800

    Added: deliverables 3,4,5 TODO: deliverables 6,7,8,11
    Fields added: year to season. seasonID to game. new entity GameGoal

--- app/queries/main.py
+++ app/queries/main.py
@@ -64,6 +64,24 @@ def select_athlete():
     return jsonify({'entries': json_data})
 
 
+@queries.route('/join_3_query', methods=['GET', 'POST'])
+@gzipped
+def join_3_query():
+    """Finds all players from country X who scored at least one goal
+    in a game played in Y city and Z year"""
+    desired_country = "Brazil"
+    desired_gameDest = "Paris"
+    desired_gameYear = "Year"
+
+    sql = text('''SELECT * FROM Athlete a, GameGoal gg, Game g, Season s WHERE 
+            a.id = gg.athleteID AND gg.gameID = g.gameID AND s.seasonID = g.seasonID AND a.countryID = ''' + desired_country +
+               ''' AND s.year = ''' + desired_gameYear + ''' AND s.location = ''' + desired_gameDest)
+
+    data = db.engine.execute(sql)
+    a_data = [list(row) for row in data]
+
+    return jsonify({'entries': helper.select_athlete_table(data=a_data)})
+
 """ INSERT QUERIES """
 
 
@@ -94,3 +112,71 @@ def insert_query():
         'table': query_table,
         'Code': 200
     })
+
+
+""" DELETE QUERIES """
+
+
+@queries.route('/delete_query', methods=['GET', 'POST'])
+@gzipped
+def delete_query():
+    """can delete from any table"""
+    """Athlete, Coach, Competition, Game, Season, Stadium, or Team"""
+
+    query_table = "placeholder for table selected"
+    main_pk = "placeholder for pk insert field"
+
+    if query_table == "Athlete":
+        sql = text('''DELETE FROM Athlete WHERE id = ''' + main_pk)
+    elif query_table == "Coach":
+        sql = text('''DELETE FROM Coach WHERE id = ''' + main_pk)
+    elif query_table == "Competition":
+        sql = text('''DELETE FROM Competition WHERE name = ''' + main_pk)
+    elif query_table == "Game":
+        sql = text('''DELETE FROM Game WHERE gameID = ''' + main_pk)
+    elif query_table == "Season":
+        sql = text('''DELETE FROM Season WHERE seasonID = ''' + main_pk)
+    elif query_table == "Stadium":
+        composite_pk_additional = "placeholder for composite fields"
+        sql = text('''DELETE FROM Stadium WHERE name = ''' + main_pk + ''' AND location = ''' + composite_pk_additional)
+    elif query_table == "Team":
+        sql = text('''DELETE FROM Team WHERE teamID = ''' + main_pk)
+    else:
+        return jsonify({'error': "Invalid Table Name"})
+
+    db.engine.execute(sql)
+
+    return jsonify({
+        'query_type': 'DELETE',
+        'table': query_table,
+        'Code': 200
+    })
+
+""" UPDATE QUERIES """
+
+
+@queries.route('/update_player', methods=['GET', 'POST'])
+@gzipped
+def update_player():
+    """updates the stats of a certain player"""
+
+    goals = 10
+    assists = 12
+    wins = 50
+    losses = 50
+    main_pk = 1000
+
+    sql = text('''UPDATE Player SET goals = ''' + goals + ''', assists = ''' + assists
+               + ''', wins = ''' + wins + ''', losses = ''' + losses + ''' WHERE id = ''' + main_pk)
+
+    db.engine.execute(sql)
+
+    return jsonify({
+        'query_type': 'UPDATE',
+        'table': "Athlete",
+        'Code': 200
+    })
+
+
+
+
